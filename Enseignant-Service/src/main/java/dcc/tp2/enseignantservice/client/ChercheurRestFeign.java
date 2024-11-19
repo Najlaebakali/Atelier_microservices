2bfc531cf87d2c4bca857f5d2448ba2aca0aa1ff
Najlaebk <bakalinajlae83@gmail.com>
2024-11-19 16:19:56 +0100

    changes were mada

--- Enseignant-Service/src/main/java/dcc/tp2/enseignantservice/client/ChercheurRestFeign.java
+++ Enseignant-Service/src/main/java/dcc/tp2/enseignantservice/client/ChercheurRestFeign.java
@@ -1,5 +1,9 @@
 package dcc.tp2.enseignantservice.client;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Getter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ChercheurRestFeign {
 
     @GetMapping("/Chercheurs/Enseignant/{id}")
+    //@CircuitBreaker(name ="count-chercheur",fallbackMethod = "Chercheur_fallbackMethod")
+    @Retry(name = "count-chercheur",fallbackMethod = "Chercheur_fallbackMethod")
+    //@RateLimiter(name = "count_chercheur",fallbackMethod = "Chercheur_fallbackMethod")
+    //@Bulkhead(name = "count-chercheur",fallbackMethod ="Chercheur_fallbackMethod")
     Long nb_chercheur_Enseignant(@PathVariable Long id);
+
+    default Long Chercheur_fallbackMethod(Long id,Exception e){
+        return null;
+    }
 
 }
--- Enseignant-Service/src/main/java/dcc/tp2/enseignantservice/client/ProjetRestFeign.java
+++ Enseignant-Service/src/main/java/dcc/tp2/enseignantservice/client/ProjetRestFeign.java
@@ -1,5 +1,8 @@
 package dcc.tp2.enseignantservice.client;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProjetRestFeign {
 
     @GetMapping("/Projets/Enseignant/{id}")
+    //@CircuitBreaker(name ="count-projet",fallbackMethod = "Projet_fallbackMethod")
+    @Retry(name = "count-projet",fallbackMethod = "Projet_fallbackMethod")
+    //@RateLimiter(name = "count_projet",fallbackMethod = "Projet_fallbackMethod")
+    //@Bulkhead(name = "count-projet",fallbackMethod ="Projet_fallbackMethod")
     Long nb_Projet_Enseignant(@PathVariable Long id);
 
+    default Long Projet_fallbackMethod(Long id,Exception e){
+        return null;
+    }
 }
